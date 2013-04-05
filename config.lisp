0009ebb8a59fa92a3eabb13e414a45f1c478261d
Joe Taylor <joekarma@gmail.com>
2013-04-04 18:32:59 -0700

    Added functions for listing configuration files, setting configuration values, and deleting configuration files.

--- config.lisp
+++ config.lisp
@@ -6,6 +6,15 @@
   (ensure-directories-exist
    (merge-pathnames ".config/common-lisp/" (user-homedir-pathname))))
 
+(defun list-configuration-files ()
+  (let ((configuration-files '()))
+    (fad:walk-directory (get-configurations-directory)
+                        (lambda (f)
+                          (when (string= (pathname-type f)
+                                         "conf")
+                            (push f configuration-files))))
+    configuration-files))
+
 (defun get-configuration-file-path (system)
   (merge-pathnames (make-pathname :name (string-downcase (string system)) :type "conf")
                    (get-configurations-directory)))
@@ -15,9 +24,22 @@
         (*print-pretty* t))
     (alexandria:write-string-into-file (write-to-string configurations-alist) path)))
 
+(defun set-configuration (system key value)
+  (let ((config (get-configuration system)))
+    (setf config (delete-if (alexandria:curry #'eql key) config :key #'car))
+    (alexandria:write-string-into-file (write-to-string (cons (cons key value) config))
+                                       (get-configuration-file-path system)
+                                       :if-exists :overwrite)))
+
 (defun get-configuration (system &optional key)
   (with-open-file (config (get-configuration-file-path system))
     (let ((result (read config)))
       (if key
           (alexandria:assoc-value result key)
           result))))
+
+(defun delete-configuration-file (system)
+  (ignore-errors
+    (delete-file (get-configuration-file-path system))))
+
+
--- package.lisp
+++ package.lisp
@@ -6,5 +6,7 @@
    #:get-configuration
    #:make-configuration-file
    #:get-configuration-file-path
-   #:get-configurations-directory))
+   #:get-configurations-directory
+   #:list-configuration-files
+   #:set-configuration))
 
